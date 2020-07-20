d282d4b76864a4cabf376ab13bfed0fa751aaa0a
Sunday <nsoooooooooosn@gmail.com>
2020-07-21 00:00:09 +0700

    :lipstick: menu responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = (props) => {
     const { scrollToRef, refProfile, refSkills, refEducational, refWorkExperience } = props
 
     const [select, setSelect] = useState('profile')
+    const [menu, setMenu] = useState(false)
 
     return (
         <div className="navbar">
@@ -13,6 +16,7 @@ const Navbar = (props) => {
                 </div>
             </div>
             <div className="menu">
+
                 <div
                     className={`list ${select === 'profile' ? 'active' : ''}`}
                     onClick={() => { scrollToRef(refProfile); setSelect('profile') }}
@@ -29,6 +33,30 @@ const Navbar = (props) => {
                     className={`list ${select === 'work experience' ? 'active' : ''}`}
                     onClick={() => { scrollToRef(refWorkExperience); setSelect('work experience') }}
                 >Work Experience</div>
+
+            </div>
+            <div className="menu-mobile">
+                <FontAwesomeIcon icon={faBars} onClick={() => setMenu(!menu)} />
+                <div className={`list-mobile ${menu ? "" : "close"}`} >
+
+                    <div
+                        className={`list ${select === 'profile' ? 'active' : ''}`}
+                        onClick={() => { scrollToRef(refProfile); setSelect('profile') }}
+                    >Profile</div>
+                    <div
+                        className={`list ${select === 'educational' ? 'active' : ''}`}
+                        onClick={() => { scrollToRef(refEducational); setSelect('educational') }}
+                    >Educational</div>
+                    <div
+                        className={`list ${select === 'skills' ? 'active' : ''}`}
+                        onClick={() => { scrollToRef(refSkills); setSelect('skills') }}
+                    >Skills</div>
+                    <div
+                        className={`list ${select === 'work experience' ? 'active' : ''}`}
+                        onClick={() => { scrollToRef(refWorkExperience); setSelect('work experience') }}
+                    >Work Experience</div>
+
+                </div>
             </div>
         </div>
     )
