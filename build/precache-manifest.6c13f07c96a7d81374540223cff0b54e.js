a22ed9e4f3eca999f758c543f61469c01fb06b7a
Sunday <nsoooooooooosn@gmail.com>
2020-07-23 14:34:29 +0700

    :rocket: push build

---
+++ build/precache-manifest.6c13f07c96a7d81374540223cff0b54e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2fd6ea052e0a553c9ada8cd75435e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e22f28b640c7827c79f",
+    "url": "/static/css/main.a0397a4c.chunk.css"
+  },
+  {
+    "revision": "4d197ae7604a50a2668b",
+    "url": "/static/js/2.51a086f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51a086f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e22f28b640c7827c79f",
+    "url": "/static/js/main.ff104469.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "11acc5f06095c63c914a6985e7af1ac7",
+    "url": "/static/media/d2x.11acc5f0.png"
+  },
+  {
+    "revision": "69bf5851d2cd197468c97257de32bd87",
+    "url": "/static/media/nimble.69bf5851.png"
+  },
+  {
+    "revision": "ab1bd0e9ef7c87c986d30c54ddad4ba9",
+    "url": "/static/media/phuket-local-explore.ab1bd0e9.png"
+  },
+  {
+    "revision": "7a002b68f1309f9e1c82618638b3cff3",
+    "url": "/static/media/plearnbook.7a002b68.png"
+  },
+  {
+    "revision": "47fc248e7c43e3c40ae893e61455d013",
+    "url": "/static/media/profile.47fc248e.jpg"
+  }
+]);
